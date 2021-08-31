3e5ad816ca50f16093d5adb58203a4c40368c274
Mehmet Aziz Algüllü <3363217433MAziz28666774646>
2021-08-31 17:08:33 +0300

    added codes related to socketApi component and Palette component. Color data from input sent to backend(socket.io-client). Received color data is displayed to all users.

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import Palette from './components/Palette'
+import { init, subscribe } from './socketApi'
+import { useEffect, useState } from 'react'
+
 
 function App() {
+
+  const [activeColor, setActiveColor] = useState("#282c34");
+
+  //didMount anı dedikleri bu. component yerleştiğinde demek oluyor.
+  useEffect(() => {
+    init(); //component yerleşmişse servere girebiliriz.
+    subscribe((color) => {
+      setActiveColor(color);
+    }); //aboneliğimizi başlattık.
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ backgroundColor: activeColor }}>
+      <Palette activeColor={activeColor} />
     </div>
   );
 }
